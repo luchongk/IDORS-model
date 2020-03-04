f94adc06b49e1761c77d6ff34250dada372e9e49
luchongk <luchong12@gmail.com>
2020-03-03 21:08:51 -0300

    Add autofetch hate votes from server and count ambiguous

--- data_mgmt/build_ds.py
+++ data_mgmt/build_ds.py
@@ -1,10 +1,23 @@
 import krippendorff, csv, sys
 import pprint as pp
 import numpy as np
+import paramiko
+import base64
 
 dataFileName = sys.argv[1]
+
+client = paramiko.SSHClient()
+client.load_system_host_keys()
+client.connect('odioelodio.com', username=f'{sys.argv[2]}')
+stdin, stdout, stderr = client.exec_command(f'mysql -u test -p{sys.argv[3]} -e "use pgodio;select v.tweet_id, t.text, count(v.is_hateful) as count, v.is_hateful from tweets t right join votesIsHateful v on t.id = v.tweet_id group by v.tweet_id, v.is_hateful;"')
+with open(dataFileName, "w") as tsvFile:
+    tsvFile.writelines(stdout)
+client.close()
+
 valueCounts = {}
 ids = set()
+
+countAmbiguous = 0
 
 with open(dataFileName) as tsvFile:
     tsvReader = csv.DictReader(tsvFile, delimiter="\t")
@@ -23,6 +36,8 @@ with open(dataFileName) as tsvFile:
                 continue
             ids.add(line['tweet_id'])
             counts = valueCounts[line['tweet_id']]
+            if abs(counts[0] - counts[1]) <= 1:
+                countAmbiguous += 1
             if counts[0] == counts[1]:
                 print(line['text'])
                 continue
@@ -30,7 +45,8 @@ with open(dataFileName) as tsvFile:
             writer.writerow({'id': line['tweet_id'], 'text': line['text'], 'HS': str(label)})
             
 
-print("\nNominal:", krippendorff.alpha(value_counts=np.array(list(valueCounts.values())), level_of_measurement='nominal'))
+print("\nAmbiguous:", countAmbiguous)
+print("\nKrippendorff:", krippendorff.alpha(value_counts=np.array(list(valueCounts.values())), level_of_measurement='nominal'))
 # pp.pprint(list(filter(lambda v : abs(v[0] - v[1]) > 1, valueCounts)))
 
 
