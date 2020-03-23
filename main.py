169845607659f2059385eafb10a0f134a596f50c
luchongk <luchong12@gmail.com>
2020-03-23 20:54:55 -0300

    Fix typo

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ training_dataset_text, test_dataset_text = get_dataset()
 
 ########## Train and Test Process ########## 
 try:
-    ft_model = load_model("modesl/fasttext_model/baseline.bin")
+    ft_model = load_model("models/fasttext_model/baseline.bin")
 except ValueError as err:
     print(err)
     print("Couldn't find a saved model, aborting...")
