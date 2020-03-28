babe21b9674bdc298127fc0e390e1a1f994dfa3d
manuel-saravia <manuel.saravia@fing.edu.uy>
2020-03-28 18:53:04 -0300

    Add logging functions

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-import sys
-import models.fasttext_model.baseline as baseline_model
+import sys, time, math
 import numpy as np
 import tensorflow as tf
-from models.functional_model import FunctionalModel
 
-from data_mgmt.data_mgmt import new_dataset, get_dataset, dataset_to_embeddings
 from fasttext import load_model
+from pprint import pprint
+from pathlib import Path
 
+from models.fasttext_model import baseline as baseline_model
+from models.functional_model import FunctionalModel
 from models.tf_model import TfModel
+from data_mgmt.data_mgmt import new_dataset, get_dataset, dataset_to_embeddings
+
+EPOCHS = 400
 
 def labelProportion(trainLabels, testLabels):
     countYesTraining = 0
@@ -40,6 +44,9 @@ retrain = True if '--retrain' in sys.argv else False
 save = True if '--save' in sys.argv else False
 functional = True if '--functional' in sys.argv else False
 training_set_ratio = float(sys.argv[-1])
+
+# Logging parameters
+skipLogging = True if '--skip-logging' in sys.argv else False
 
 # TODO: Change resplit parameter
 if resplit:
@@ -113,28 +120,45 @@ else:
 
 if retrain:
     if (functional):
-        print(model.summary())
+        model.summary()
         
         earlyStopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=8,
                                                     restore_best_weights=True)
         
-        model.fit(training_dataset_embeddings, training_dataset_labels,
+        history = model.fit(training_dataset_embeddings, training_dataset_labels,
                     batch_size=64,
-                    epochs=400,
+                    epochs=EPOCHS,
                     validation_split=0.2,
                     callbacks=[earlyStopping])
 
         #TODO: Make an evaluate method for the subclassed model
         loss, accuracy, precision, recall, auc = model.evaluate(test_dataset_embeddings, test_dataset_labels, verbose=2)
 
-        print('Test loss:', loss)
-        print('Test accuracy:', accuracy)
-        print('Test precision:', precision)
-        print('Test recall:', recall)
-        print('Test AUC:', auc)
-        print('Test F-Score', 2 * (precision * recall) / (precision + recall))
+        fscore = 2 * (precision * recall) / (precision + recall)
+
+        template = '\n###### Test results ######\n\nTest Loss: {},\nTest Accuracy: {},\nTest Precision: {},\nTest Recall: {},\nTest AUC: {},\nTest F-Score: {}\n'
+        
+        print(template.format(loss, accuracy, precision, recall, auc, fscore))
+
+        if not skipLogging:
+            Path("runs").mkdir(parents=True, exist_ok=True)
+            with open('runs/' + dataset_tsv_file.split('.tsv')[0] + str(math.trunc(time.time())), 'w') as logfile:
+                logfile.write('Using dataset: ' + dataset_tsv_file + '\n')
+                logfile.write('\n###### Model Summary ######\n\n'.format(EPOCHS))
+                model.summary(print_fn=lambda x: logfile.write(x + '\n'))
+                logfile.write(template.format(loss, accuracy, precision, recall, auc, fscore))
+                logfile.write('\n###### Metrics history for {} epochs: ######\n\n'.format(len(history.epoch)))
+                for epoch in history.epoch:
+                    metricsHistory = history.history
+                    logfile.write('Epoch {}: '.format(epoch + 1))
+                    for key in metricsHistory.keys():
+                        logfile.write('{}: {},'.format(key, metricsHistory[key][epoch]))
+                        logfile.write(' ')
+                    logfile.write('\n')
+                logfile.write('\n##### Raw metrics history #####\n\n')
+                pprint(metricsHistory, logfile)
     else:
-        model.fit(training_dataset, test_dataset, 100)
+        model.fit(training_dataset, test_dataset, EPOCHS)
 else:
     try:
         model.load_weights('tf_weights.h5')
