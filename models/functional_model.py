0af7696cf00e0970cbd96918cdacd731abe87c0a
manuel-saravia <manuel.saravia@fing.edu.uy>
2020-04-13 23:09:06 -0300

    Add bert layer and extra embeddings layer

--- models/functional_model.py
+++ models/functional_model.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
+import bert, os, tensorflow as tf
 
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -9,7 +9,7 @@ def tass(inputs):
 
     return layers.Dropout(.2)(x)
 
-def pereira_kohatsu(inputs):
+def haternet(inputs):
     droppedInputs = layers.Dropout(.8)(inputs)
 
     d1 = layers.Dense(1600, activation='relu')(droppedInputs)
@@ -19,6 +19,15 @@ def pereira_kohatsu(inputs):
     d2 = layers.Dense(100, activation='relu')(d1) 
 
     return layers.Dropout(.4)(d2)
+
+def get_bert_layer():
+    current_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    bert_model_dir = os.path.join(current_dir, "models/bert_model", "multi_cased_L-12_H-768_A-12")
+
+    bert_params = bert.params_from_pretrained_ckpt(bert_model_dir)
+    l_bert = bert.BertModelLayer.from_params(bert_params, name="bert")
+    
+    return l_bert
 
 #### Sentence embedding generators ####
 def conv_tass(inputs):
@@ -41,20 +50,40 @@ def custom(inputs):
 
     return layers.Dense(400, activation='relu')(concat)
 
-def FunctionalModel(inputShape, batchSize):
-    inputs = keras.Input(shape=inputShape)
+def FunctionalModel(inputShape, input2Shape, bertInputShape, use_bert):
+    inputs = keras.Input(shape=inputShape, name='tweet_word_vectors')
+    
+    inputs2 = keras.Input(shape=input2Shape, name='tweet_vectors')
+
+    input_array = [inputs, inputs2]
+
     norm = layers.BatchNormalization()(inputs)
 
     processed_inputs = conv_tass(norm)
 
-    final = tass(processed_inputs)
+    tweet_vector_array = [processed_inputs, inputs2]
+
+    if (use_bert):
+        inputs3 = keras.Input(shape=bertInputShape, name='bert_token_ids')
+
+        input_array.append(inputs3)
+
+        bert_layer = get_bert_layer()
+        bert_vectors = bert_layer(inputs3)
+        bert_vectors = keras.layers.Lambda(lambda seq: seq[:, 0, :])(bert_vectors)
+
+        tweet_vector_array.append(bert_vectors)
+
+    tweet_vector = layers.concatenate(tweet_vector_array, axis=1)
+
+    final = haternet(tweet_vector)
 
     output = layers.Dense(1, activation="sigmoid")(final)
 
-    model = keras.Model(inputs=inputs, outputs=output, name="main_output")
+    model = keras.Model(inputs=input_array, outputs=output, name="main_output")
 
     model.compile(loss=tf.keras.losses.BinaryCrossentropy(),
-                optimizer=tf.keras.optimizers.Adam(learning_rate=0.000001),
+                optimizer=tf.keras.optimizers.Adam(),
                 metrics=['accuracy', tf.keras.metrics.Precision(), tf.keras.metrics.Recall(), tf.keras.metrics.AUC()])
 
     return model
